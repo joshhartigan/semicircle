9ec2382de5a3d5377fee03a6151e5afbf36f8e71
Josh <joshhartigan99@gmail.com>
2014-06-07 16:05:08 +0100

    add doc link

--- code/mode.py
+++ code/mode.py
@@ -1,3 +1,7 @@
+# an in-depth rundown of this program
+# can be found at:
+# https://github.com/joshhartigan/learn-programming/blob/master/Most%20Frequent%20Integer.md
+
 def mode(array):
     count = {}
     for elem in array:
