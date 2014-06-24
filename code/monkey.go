0f3965eacb030d4b6d29b60c47ba10df799d0ce7
Josh <joshhartigan99@gmail.com>
2014-06-24 20:56:11 +0100

    full variable support

--- code/monkey.go
+++ code/monkey.go
@@ -8,17 +8,6 @@ import (
     "strconv"
 )
 
-var variables map[string]int
-
-// determine whether a token is a variable reference or
-// an integer literal, and return it.
-func getvalue(item string) int {
-    if num, err := strconv.Atoi(item); err == nil {
-        return num
-    }
-    return variables[item]
-}
-
 // error management
 func check(e error) {
     if e != nil {
@@ -40,26 +29,48 @@ func openfile(filename string) (lines []string) {
     return
 }
 
+var variables map[string]float32
+
+// determine whether a token is a variable reference or
+// an integer literal, and return it.
+func getvalue(item string) float32 {
+    if num, err := strconv.Atoi(item); err == nil {
+        return float32(num)
+    }
+    return variables[item]
+}
+
+func operate(operandA, operandB string, op string) float32 {
+    if op == "+" {
+        x, y := getvalue(operandA), getvalue(operandB)
+        return float32(x + y)
+    } else if op == "-" {
+        x, y := getvalue(operandA), getvalue(operandB)
+        return float32(x - y)
+    } else if op == "*" {
+        x, y := getvalue(operandA), getvalue(operandB)
+        return float32(x * y)
+    } else if op == "/" {
+        x, y := getvalue(operandA), getvalue(operandB)
+        var z float32 = float32(x) / float32(y)
+        return z
+    } // else:
+    return 0
+}
+
 var x, y int64 // these will be assigned to function params
 var xerr, yerr error
 func main() {
+    variables = make(map[string]float32)
     lines := openfile(os.Args[1])
     for _, line := range lines {
         words := strings.Split(line, " ")
-        // convert function parameters to integer types and operate on them
-        if words[0] == "+" {
-            x, y := getvalue(words[1]), getvalue(words[2])
-            fmt.Println(x + y)
-        } else if words[0] == "-" {
-            x, y := getvalue(words[1]), getvalue(words[2])
-            fmt.Println(x - y)
-        } else if words[0] == "*" {
-            x, y := getvalue(words[1]), getvalue(words[2])
-            fmt.Println(x * y)
-        } else if words[0] == "/" {
-            x, y := getvalue(words[1]), getvalue(words[2])
-            var z float32 = float32(x) / float32(y)
-            fmt.Println(z)
+        if len(words) == 3 {
+            fmt.Println(operate(words[1], words[2], words[0]))
+        } else if len(words) == 5 {
+            if words[3] == ":" {
+                variables[words[4]] = operate(words[1], words[2], words[0])
+            }
         }
     }
 }
