95262b709f776394ff16ed1cb3e91c5450384cdc
Josh <joshhartigan99@gmail.com>
2014-06-26 22:40:00 +0100

    monkey now accepts >= 2 parameters

--- code/monkey.go
+++ code/monkey.go
@@ -34,6 +34,7 @@ var variables map[string]float32
 // determine whether a token is a variable reference or
 // an integer literal, and return it.
 func getvalue(item string) float32 {
+    // if item is number
     if num, err := strconv.Atoi(item); err == nil {
         return float32(num)
     }
@@ -41,32 +42,38 @@ func getvalue(item string) float32 {
 }
 
 func operate(operands []string, op string) float32 {
-    result := 0
+    var result float32 = 0
     // o = index in operands[]
     for o := 0; o < len(operands); o++ {
         if op == "+" {
-            if o == 0 { result := operands[0] }
-            else {
-                result += operands[o]
+            if o == 0 {
+                result = getvalue(operands[0])
+            } else {
+                result += getvalue(operands[o])
             }
         } else if op == "-" {
-            if o == 0 { result := operands[0] }
-            else {
-                result -= operands[o]
+            if o == 0 {
+                result = getvalue(operands[0])
+            } else {
+                result -= getvalue(operands[o])
             }
         } else if op == "*" {
-            if o == 0 { result := operands[0] }
-            else {
-                result *= operands[o]
+            if o == 0 {
+                result = getvalue(operands[0])
+            } else {
+                result *= getvalue(operands[o])
             }
         } else if op == "/" {
-            if o == 0 { result := operands[0] }
-            else {
-                result /= operands[o]
+            if o == 0 {
+                result = getvalue(operands[0])
+            } else {
+                result /= getvalue(operands[o])
             }
+        } else {
+            return 0.0
         }
     }
-    return float32(result)
+    return result
 }
 
 var x, y int64 // these will be assigned to function params
@@ -74,16 +81,24 @@ var xerr, yerr error
 func main() {
     variables = make(map[string]float32)
     lines := openfile(os.Args[1])
-    for _, line := range lines {
+    for lineNumber, line := range lines {
         words := strings.Split(line, " ")
-        // expressions are printed
-        if words[len(words)-1] == ";" {
-           fmt.Println(operate(, words[0]))
-        } else {
-            // variable assignment
-            if words[len(words)-2] == ":" {
-                variables[words[len(words)-1]] = operate(words[1], words[2], words[0])
+        operands := 0 // count of numbers that the operation will be applied to
+        for _, word := range words {
+            if _, err := strconv.Atoi(word); err == nil {
+                operands += 1
             }
+        }
+        // line needs to end in ';' or ': varName' to be correct
+        if len(words) >= operands + 2 {
+            if words[operands + 1] == ";" {
+                fmt.Println(operate(words[1:operands+1], words[0]))
+            } else if words[operands + 1] == ":" {
+                fmt.Println("var", words[operands + 2], "=", operate(words[1:operands+1], words[0]))
+                variables[words[operands + 2]] = operate(words[1:operands+1], words[0])
+            }
+        } else if len(words) < 0 && len(words) < operands + 2 {
+            fmt.Println("error: not enough arguments at line", lineNumber)
         }
     }
 }
