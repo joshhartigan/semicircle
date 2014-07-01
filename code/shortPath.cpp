e24cd38ec4da7a564bce6acb73ce50e65f615fc1
= <=>
2014-07-01 21:46:10 +0100

    diagonal path finding

--- code/shortPath.cpp
+++ code/shortPath.cpp
@@ -4,15 +4,35 @@
 //              to get from the first point to the other, in a situation
 //              where the movement must be straight and non-diagonal.
 
+#define _USE_MATH_DEFINES // this is for the pi constant
+
 #include <iostream>
 #include <array>
+#include <cmath>
+#include <string>
 
 std::array<int, 2> getShortestPath(int x1, int y1, int x2, int y2) {
-  std::array<int, 2> vec = {{
-    x2 - x1 + 1,
-    y2 - y1 + 1
-  }};
+  std::array<int, 2> vec = {{ x2 - x1, y2 - y1 }};
   return vec;
+}
+
+std::array<double, 2> diagonalPath(int x1, int y1, int x2, int y2) {
+  double angle;     // angle in degrees of the diagonal path
+  double distance;  // the actual length of the path
+
+  // calculate distance
+  distance = hypot((x2 - x1), (y2 - y1));
+  if (x1 > x2) {
+    distance *= -1; // moving from right to left is negative
+  }
+
+  // calculate angle -- the angle at the first point
+  double xdiff = std::abs(x1 - x2);
+  double ydiff = std::abs(y1 - y2);
+  angle = atan(xdiff / ydiff) * 180 / M_PI;
+
+  std::array<double, 2> path = {{ distance, angle }};
+  return path;
 }
 
 int main() {
@@ -33,7 +53,19 @@ int main() {
   std::cout << "\tY-Position? - ";
   std::cin >> secondPointY;
 
-  std::array<int, 2> path = getShortestPath(firstPointX, firstPointY, secondPointX, secondPointY);
-  // print values of path
-  std::cout << "\nShortest non-diagonal path: (" << std::get<0>(path) << ", " << std::get<1>(path) << ")\n";
+  std::string allowDiagonal;
+  std::cout << "\nAllow Diagonal? (Y/n) - ";
+  std::cin >> allowDiagonal;
+
+  if (allowDiagonal == "n") {
+    std::array<int, 2> path = getShortestPath(firstPointX, firstPointY, secondPointX, secondPointY);
+    // print values of path
+    std::cout << "\nShortest non-diagonal path: (" << std::get<0>(path) << ", " << std::get<1>(path) << ")\n";
+  } else {
+    std::array<double, 2> path = diagonalPath(firstPointX, firstPointY, secondPointX, secondPointY);
+    // print values of path
+    std::cout << "\nShortest diagonal path: length of " << std::get<0>(path) << ", at " << std::get<1>(path) << " degrees.\n";
+  }
+
+  return 0;
 }
