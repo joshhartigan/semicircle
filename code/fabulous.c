de2ce9c74d1512def4cd4fecc71e8ee31b828926
Josh Hartigan <joshhartigan99@gmail.com>
2015-02-06 22:05:58 +0000

    Create fabulous.c

---
+++ code/fabulous.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <curses.h>
+
+void init_colors() {
+  init_pair(1,  COLOR_RED,     COLOR_BLACK);
+  init_pair(2,  COLOR_GREEN,   COLOR_BLACK);
+  init_pair(3,  COLOR_YELLOW,  COLOR_BLACK);
+  init_pair(4,  COLOR_BLUE,    COLOR_BLACK);
+  init_pair(5,  COLOR_MAGENTA, COLOR_BLACK);
+  init_pair(6,  COLOR_CYAN,    COLOR_BLACK);
+  init_pair(7,  COLOR_BLUE,    COLOR_WHITE);
+  init_pair(8,  COLOR_WHITE,   COLOR_RED);
+  init_pair(9,  COLOR_BLACK,   COLOR_GREEN);
+  init_pair(10, COLOR_BLUE,    COLOR_YELLOW);
+  init_pair(11, COLOR_WHITE,   COLOR_BLUE);
+  init_pair(12, COLOR_WHITE,   COLOR_MAGENTA);
+  init_pair(13, COLOR_BLACK,   COLOR_CYAN);
+}
+
+int main() {
+  WINDOW *window;
+
+  if ((window = initscr()) == NULL) {
+    fprintf(stderr, "error initialising curses.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  start_color();
+
+  if (has_colors() && COLOR_PAIRS >= 13) {
+    init_colors();
+    for (int i = 0; i < 13; i++) {
+      color_set(i, NULL);
+      mvaddstr(6 + i, 32, "fabulous");
+    }
+  }
+
+  refresh();
+  //sleep(3);
+  getch();
+
+  delwin(window);
+  endwin();
+  refresh();
+}
