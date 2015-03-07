ae3fa8e78dfd18461a432810af67503ddcbefc47
Josh Hartigan <joshhartigan99@gmail.com>
2015-03-07 15:34:09 +0000

    more rust features shown in try-rust

--- code/try-rust.rs
+++ code/try-rust.rs
@@ -1,3 +1,6 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
 fn main() {
     greet_the_world();
 
@@ -6,14 +9,14 @@ fn main() {
     // * they are both immutable.
     // * therefore, they must be initialised
     //   immediately
-    let x = 5i;
-    let y:int = 5;
+    let x = 5;
+    let y:i32 = 5;
 
     // this declares a mutable variable:
-    let mut z = 10i;
-    z = 100i; // tada!
+    let mut z = 10;
+    z = 100; // tada!
 
-    if z >= 50i {
+    if z >= 50 {
         println!("z is a big number");
     } else {
         println!("z isn't a big number");
@@ -23,13 +26,25 @@ fn main() {
     let a = if x > 1 { true } else { false };
     // (these need semicolons)
 
-    describe_number(200i);
-    describe_number(23i);
+    describe_number(200);
+    describe_number(23);
+
+    iterations();
+
+//  fn  square   (x:i32) { x * x }
+    let square = |x:i32| { x * x };
+    // see the `twice()` function for a 'proper'
+    // use of rust's closures
+
+    println!("the square of {} is {}", 17, square(17));
+    println!("twice the square of {} is {}", 29, twice(29, square));
 }
 
-// argument types must be specified
-// ... and so must return types
-fn describe_number(number: int) -> int {
+fn twice<F: Fn(i32) -> i32>(x: i32, f: F) -> i32 {
+    f(x) + f(x)
+}
+
+fn describe_number(number: i32) -> i32 {
     println!("the number is {}", number);
 
     let big = if number > 50 { "is big" } else { "isn't big" };
@@ -40,6 +55,14 @@ fn describe_number(number: int) -> int {
            // would look for another expression to return
            // (and there isn't one, thrfr. the return type
            // would be a lie)
+}
+
+fn iterations() {
+    let arr = [3, 8, 9, 2, 3, 0, 1, 8, 5, 4];
+    for element in arr[0..arr.len()].iter() {
+        print!("{}, ", element);
+    }
+    println!("{}", arr[arr.len() -1 ]);
 }
 
 // * what follows is a doc comment (notice the triple slash):
@@ -63,4 +86,3 @@ fn describe_number(number: int) -> int {
 fn greet_the_world() {
     println!("hello, world");
 }
-
