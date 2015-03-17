ba04591d795bbc2dd8df5c94e464de354c9e2fd5
Josh Hartigan <joshhartigan99@gmail.com>
2015-03-17 18:37:30 +0000

    improvement to the "upto.c" program

--- code/upto.c
+++ code/upto.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-#define UPTO(i, x) i < x; i++
+#define upto(x, y) for (int x = 0; x < y; ++x)
 
 int main() {
   int numbers[] = { 3, 6, 2, 9, 8, 1, 0, 4, 5 };
   int length = sizeof(numbers) / sizeof(int);
 
-  for (int i = 0; UPTO(i, length)) {
+  upto(i, length) {
     printf("%d ", numbers[i]);
   }
 }
