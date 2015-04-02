5138de0e8bebf0fd80c74616e5b7e4691b050b76
Josh Hartigan <joshhartigan99@gmail.com>
2015-04-02 20:06:08 +0100

    Create gui-test.go

---
+++ code/gui-test.go
@@ -0,0 +1,37 @@
+package main
+
+import "github.com/andlabs/ui"
+
+var window ui.Window
+
+func main() {
+  go ui.Do(func() {
+    name := ui.NewTextField()
+    button := ui.NewButton("greet")
+    greeting := ui.NewLabel("")
+
+    stack := ui.NewVerticalStack(
+      ui.NewLabel("name:"),
+      name,
+      button,
+      greeting)
+
+    window = ui.NewWindow("hello world", 200, 100, stack)
+
+    button.OnClicked(func() {
+      greeting.SetText("hello, " + name.Text() + "!")
+    })
+
+    window.OnClosing(func() bool {
+      ui.Stop()
+      return true
+    })
+
+    window.Show()
+  })
+
+  err := ui.Go()
+  if err != nil {
+    panic(err)
+  }
+}
