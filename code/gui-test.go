817674776d9c2c85541add84d0ea49f8874def6b
Josh Hartigan <joshhartigan99@gmail.com>
2015-04-02 20:06:53 +0100

    go fmt gui-test.go

--- code/gui-test.go
+++ code/gui-test.go
@@ -5,33 +5,33 @@ import "github.com/andlabs/ui"
 var window ui.Window
 
 func main() {
-  go ui.Do(func() {
-    name := ui.NewTextField()
-    button := ui.NewButton("greet")
-    greeting := ui.NewLabel("")
+	go ui.Do(func() {
+		name := ui.NewTextField()
+		button := ui.NewButton("greet")
+		greeting := ui.NewLabel("")
 
-    stack := ui.NewVerticalStack(
-      ui.NewLabel("name:"),
-      name,
-      button,
-      greeting)
+		stack := ui.NewVerticalStack(
+			ui.NewLabel("name:"),
+			name,
+			button,
+			greeting)
 
-    window = ui.NewWindow("hello world", 200, 100, stack)
+		window = ui.NewWindow("hello world", 200, 100, stack)
 
-    button.OnClicked(func() {
-      greeting.SetText("hello, " + name.Text() + "!")
-    })
+		button.OnClicked(func() {
+			greeting.SetText("hello, " + name.Text() + "!")
+		})
 
-    window.OnClosing(func() bool {
-      ui.Stop()
-      return true
-    })
+		window.OnClosing(func() bool {
+			ui.Stop()
+			return true
+		})
 
-    window.Show()
-  })
+		window.Show()
+	})
 
-  err := ui.Go()
-  if err != nil {
-    panic(err)
-  }
+	err := ui.Go()
+	if err != nil {
+		panic(err)
+	}
 }
