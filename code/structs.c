8e8c854133abd84540deda53667bc842d43e55e5
Josh Hartigan <joshhartigan99@gmail.com>
2015-04-02 15:59:44 +0100

    additions to structs.c

--- code/structs.c
+++ code/structs.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 
-// Immutability is generally good.
-typedef const struct {
+typedef struct {
   char *name;
   int age;
   char *gender;
+  char *description;
 } person;
 
 void introduce(person per) {
   printf("Hello, I am %s.\n", per.name);
   printf("I am a %d year-old %s person.\n", per.age, per.gender);
+  printf("I am %s.\n", per.description);
+}
+
+void birthday(person *per) {
+  printf("Happy birthday, %s!\n", per->name);
+  per->age += 1;
 }
 
 int main() {
-  person me = { .name = "Josh", .age = 15, .gender = "Male" };
+  person me = { .name = "Josh", .age = 15, .gender = "Male",
+    .description = "the creator of all living and non-living things" };
   introduce(me);
 
-  person you = { .name = "a person", .age = 29, .gender = "Female" };
+  printf("\n");
+
+  person you = { .name = "a person", .age = 29, .gender = "Female",
+    .description = "the most intelligent person ever" };
   introduce(you);
+
+  printf("\n");
+
+  printf("%s is %d years old.\n", me.name, me.age);
+  birthday(&me);
+  printf("%s is now %d years old.\n", me.name, me.age);
 }
